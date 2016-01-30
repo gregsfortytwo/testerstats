bec88ff897bee42791ca29487af7da576c31fccf
Greg Farnum <gfarnum@redhat.com>
2016-01-29 17:15:18 -0800

    read in json and build a dict based on one element

--- testerstats.py
+++ testerstats.py
@@ -1,41 +1,56 @@
 #!/usr/bin/python
 import argparse
 import json
-import yaml
 from collections import defaultdict
+import sys
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Read JSON lab data and generate statistics.')
     parser.add_argument(
-        'json_file',
-        help = 'JSON output file',
+        '--json-file', dest='json_file',
+        required=True,
+        help = 'JSON file describing jobs, from pulpito',
         )
+    parser.add_argument(
+        '--suite', dest='suite',
+        required=True,
+        help = 'suite to summarize',
+        )
+    parser.add_argument(
+        '--sort-by', dest='filter_element', default='description',
+        help = 'job element to filter on'
+    )
     args = parser.parse_args()
     return args
 
-def parse_json(config, data):
+def parse_json_to_dict(data, suite, filter_element):
     try:
         json_data = json.loads(data)
     except ValueError, e:
         raise ValueError('could not parse json data')
     d = defaultdict(list)
     for record in json_data:
-        data = {}
-        total = {}
-        for value in ['status', 'job', 'nodes', 'duration', 'runtime', 'waited', 'description', 'suite']:
-            data[value] = record[value]
-        data['wallclockseconds'] = record['nodes'] * record['duration']
-        d[record['suite']].append(data)
-    return dict(d)
+        if (suite == record['suite']):
+            d[filter_element].append(record)
+
+    print "filtered out {num} results for {suite}".format(num=len(d), suite=ctx.suite)
+    return d
+
+def dump_results(data):
+    for k,v in data.iteritems():
+        print "{desc}: {results}".format(desc=k, results=v)
 
 if __name__ == '__main__':
     ctx = parse_args()
     try:
-        json_file = open(ctx.json_file).read()
+        json_stream = open(ctx.json_file).read()
     except IOError as e:
-        print 'cannot open %s' % json_file
+        print 'cannot open %s' % json_stream
+        print e
         sys.exit(1)
     try:
-        suite_data = parse_json(json_file)
+        suite_data = parse_json_to_dict(json_stream, ctx.suite, ctx.filter_element)
     except ValueError, e:
+        print e
         sys.exit(1)
+    dump_results(suite_data)
